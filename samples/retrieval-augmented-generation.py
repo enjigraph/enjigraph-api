bea0d25b26d80406d3684cba9a166dcc806937e7
enjigraph <t-yasutomi@enjigraph.com>
2023-09-21 11:31:18 +0900

    Update: retrieval-augmented-generation.py
    update prompt

--- samples/retrieval-augmented-generation.py
+++ samples/retrieval-augmented-generation.py
@@ -19,8 +19,40 @@ with urllib.request.urlopen(req) as res:
     for data in body["data"]:
         information += "title:"+str(data["title"])+" description:"+str(data["description"])+"\n"
     
-    role = "The following is a conversation with a assistant and a manager. manager: 大規模言語モデルの開発はどこまで進んでいる？最新情報に基づいて教えて。 assistant: 大規模言語モデルの開発は現在、OpenAIやGoogle、Microsoftを筆頭に開発が進められています。つい最近、OpenAIは法人向けサービスを開始しています。"
-    question = "自動運転はどこまで開発が進んでいる？次の情報も加味して教えて。"+information
+    role = """
+    #命令文
+    あなたは、プロのリサーチャーです。
+    以下の制約条件と質問をもとに的確な解答を出力してください
+    
+    #制約条件
+    ・初心者でもわかりやすく
+    ・重要なキーワードを抜けもれなく含める
+    ・具体的な事例・事実を含める
+    ・できるだけ最新の情報に基づく
+
+    #質問
+    規模言語モデルの開発はどこまで進んでいる？
+    
+    #解答
+    大規模言語モデルの開発は現在、OpenAIやGoogle、Microsoftを筆頭に開発が進められています。つい最近、OpenAIは法人向けサービスを開始しています。
+    """
+
+    question = """
+    #命令文
+    あなたは、プロのリサーチャーです。
+    以下の制約条件と質問をもとに的確な解答を出力してください
+    
+    #制約条件
+    ・初心者でもわかりやすく
+    ・重要なキーワードを抜けもれなく含める
+    ・具体的な事例・事実を含める
+    ・できるだけ最新の情報に基づく
+    ・次の情報も加味する
+    """+information+"""
+
+    #質問
+    自動運転はどこまで開発が進んでいる？
+    """
     
     completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",messages=[{"role":"system","content":role},{"role":"user","content":question}])
     print(completion.choices[0].message.content)
